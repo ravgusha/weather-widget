82864179a8887b1ce0adb7b20c0f6b524985f267
ravgusha <ravgusha@gmail.com>
2022-09-17 23:17:42 +0300

    regactor: change styles

--- src/components/CardItem/CardItem.vue
+++ src/components/CardItem/CardItem.vue
@@ -73,44 +73,29 @@ export default {
         "N",
       ];
 
-      const index = +(deg / 22.5).toFixed(0);
+      const index = Math.round(deg / 22.5);
       return compassSector[index];
     },
     getDewPoint(seaLevel: number, temperature: number, humidity: number) {
-      const xdp = new dewpoint(seaLevel);
-
-      const y = xdp.Calc(temperature, humidity);
+      const area = new dewpoint(seaLevel);
+      const y = area.Calc(temperature, humidity);
       const dewPoint = Math.floor(y.dp);
       return dewPoint;
     },
     describeWindSpeed(speed: number) {
-      if (speed < 0.3) {
-        return "calm";
-      } else if (speed >= 0.3 && speed < 1.6) {
-        return "light air";
-      } else if (speed >= 1.6 && speed < 3.4) {
-        return "light breeze";
-      } else if (speed >= 3.4 && speed < 5.5) {
-        return "gentle breeze";
-      } else if (speed >= 5.5 && speed < 8) {
-        return "moderate breeze";
-      } else if (speed >= 8 && speed < 10.8) {
-        return "fresh breeze";
-      } else if (speed >= 10.8 && speed < 13.9) {
-        return "strong breeze";
-      } else if (speed >= 13.9 && speed < 17.2) {
-        return "moderate gale";
-      } else if (speed >= 17.2 && speed < 20.8) {
-        return "gale";
-      } else if (speed >= 20.8 && speed < 24.5) {
-        return "strong gale";
-      } else if (speed >= 24.5 && speed < 28.5) {
-        return "storm";
-      } else if (speed >= 28.5 && speed < 32.7) {
-        return "violent storm";
-      } else if (speed >= 32.7 && speed < 42) {
-        return "hurricane force";
-      }
+      if (speed < 0.3) return "calm";
+      if (speed >= 0.3 && speed < 1.6) return "light air";
+      if (speed >= 1.6 && speed < 3.4) return "light breeze";
+      if (speed >= 3.4 && speed < 5.5) return "gentle breeze";
+      if (speed >= 5.5 && speed < 8) return "moderate breeze";
+      if (speed >= 8 && speed < 10.8) return "fresh breeze";
+      if (speed >= 10.8 && speed < 13.9) return "strong breeze";
+      if (speed >= 13.9 && speed < 17.2) return "moderate gale";
+      if (speed >= 17.2 && speed < 20.8) return "gale";
+      if (speed >= 20.8 && speed < 24.5) return "strong gale";
+      if (speed >= 24.5 && speed < 28.5) return "storm";
+      if (speed >= 28.5 && speed < 32.7) return "violent storm";
+      if (speed >= 32.7) return "hurricane force";
     },
   },
 };
