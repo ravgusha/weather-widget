1072b4790863652141397319b2a1d5e7f5d4f1da
ravgusha <ravgusha@gmail.com>
2022-09-19 20:30:00 +0300

    feat: add dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+const webpack = require("webpack");
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    config.optimization.delete("splitChunks");
+  },
+  filenameHashing: false,
+};
